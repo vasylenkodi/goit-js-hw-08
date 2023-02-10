593111672f2138d5f679de32acbe2a2676261a5e
vasylenkodi <vasylenko1daniil@gmail.com>
2023-02-10 13:58:30 +0100

    добавляет решение задания 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from '@vimeo/player';
 
+const vimeoPlayer = document.querySelector('#vimeo-player');
+
+const throttle = require('lodash.throttle');
+const player = new Player(vimeoPlayer);
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+
+player.on("timeupdate", throttle(onTimeUpdate, 1000));
+
+function onTimeUpdate(event) {
+    console.log(event.seconds);
+    localStorage.setItem("videoplayer-current-time", event.seconds);
+}
