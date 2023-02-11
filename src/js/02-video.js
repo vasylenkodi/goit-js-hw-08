6b3c053e201792863ffed89969a27426dcd8bcf0
vasylenkodi <vasylenko1daniil@gmail.com>
2023-02-11 18:11:48 +0100

    добавляет исправления

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -5,9 +5,9 @@ const vimeoPlayer = document.querySelector('#vimeo-player');
 const throttle = require('lodash.throttle');
 const player = new Player(vimeoPlayer);
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
 
-player.on("timeupdate", throttle(onTimeUpdate, 1000) || 0);
+player.on("timeupdate", throttle(onTimeUpdate, 1000));
 
 function onTimeUpdate(event) {
     console.log(event.seconds);
